8f1b88c951381fcd76fae28173b8754aa5fae352
La <90055383+paulagrata@users.noreply.github.com>
2024-02-23 12:42:54 -0600

    added seed [plants] switch and toggle

--- code/player.py
+++ code/player.py
@@ -27,7 +27,9 @@ class Player(pygame.sprite.Sprite):
         # timers
         self.timers = {
             'tool use': Timer(350, self.use_tool),
-            'tool switch': Timer(200)
+            'tool switch': Timer(200),
+            'seed use': Timer(350, self.use_seed),
+            'seed switch': Timer(200)
         }
 
         # tools
@@ -36,8 +38,16 @@ class Player(pygame.sprite.Sprite):
         #self.selected_tool = 'axe'
         self.selected_tool = self.tools[self.tool_index]
 
+        # seeds
+        self.seeds = ['corn', 'tomato']
+        self.seed_index = 0
+        self.selected_seed = self.seeds[self.seed_index]
+
     def use_tool(self):
         #print(self.selected_tool)
+        pass
+
+    def use_seed(self):
         pass
 
     def import_assets(self):
@@ -100,7 +110,22 @@ class Player(pygame.sprite.Sprite):
                 self.tool_index = self.tool_index if self.tool_index < len(self.tools) else 0
                 self.selected_tool = self.tools[self.tool_index]
 
+            # seed use
+            if keys[pygame.K_LCTRL]:
+                # timer for the tool use
+                self.timers['seed use'].activate()
+                self.direction = pygame.math.Vector2()
+                self.frame_index = 0
+                #print('use seed')
 
+            # change seed
+            if keys[pygame.K_e] and not self.timers['seed switch'].active:
+                self.timers['seed switch'].activate()
+                self.seed_index += 1
+                # need to restart at first seed
+                self.seed_index = self.seed_index if self.seed_index < len(self.seeds) else 0
+                self.selected_seed = self.seeds[self.seed_index]
+                #print(self.selected_seed)
 
     def get_status(self):
         #if player is not moving = set idle
