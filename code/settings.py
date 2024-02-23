fddc73ed34ff9b88a5c0ad8fd668f2334a0145da
La <90055383+paulagrata@users.noreply.github.com>
2024-02-23 12:58:58 -0600

    added overlay

--- code/settings.py
+++ code/settings.py
@@ -1,5 +1,11 @@
 from pygame.math import Vector2
 
-#screen
+# screen
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+# overlay positions
+OVERLAY_POSITIONS = {
+    'tool': (40, SCREEN_HEIGHT - 15),
+    'seed': (70, SCREEN_HEIGHT - 5)
+}
