cf392c2fce4c218f47d56e1e7f6157f495815134
La <90055383+paulagrata@users.noreply.github.com>
2024-02-23 11:39:36 -0600

    updateupdate.

--- code/poodle.py
+++ code/poodle.py
@@ -24,18 +24,3 @@ class Game:
 if __name__ == '__main__':
     game = Game ()
     game.run()
-        
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-run = True
-while run:
-
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
-
-pygame.quit()
